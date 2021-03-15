77efea0e432993ee8a83fcd90893a17893dd51e3
chernyshevdi <cllmegod@gmail.com>
2021-03-16 00:01:29 +0300

    qwe

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ((typeof array == 'undefined') || (typeof array === 'null')){
+    return 0;
+  }
+  else {
+    Math.min.apply(null, array)}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ((typeof array == 'undefined') || (typeof array === 'null')){
+    return 0;
+  }
+  else {
+  return Math.max.apply(null, array)}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ((typeof array == 'undefined') || (typeof array === 'null')) {
+    return 0;
+  }
+  else {
+  let q = array.reduce(function(sum, cur) {
+    return (sum + cur)
+  })
+  let w = array.length;
+  let e = q / w;
+  return e;}
 }
