323ba3a6d2ff7cccbe971dcc05bf56d819aeccd0
chernyshevdi <cllmegod@gmail.com>
2021-03-16 00:02:21 +0300

    ewq

--- src/index.js
+++ src/index.js
@@ -1,29 +1,20 @@
 
 exports.min = function min (array) {
-  if ((typeof array == 'undefined') || (typeof array === 'null')){
-    return 0;
-  }
-  else {
-    Math.min.apply(null, array)}
+
+    Math.min.apply(null, array)
 }
 
 exports.max = function max (array) {
-  if ((typeof array == 'undefined') || (typeof array === 'null')){
-    return 0;
-  }
-  else {
-  return Math.max.apply(null, array)}
+
+  return Math.max.apply(null, array)
 }
 
 exports.avg = function avg (array) {
-  if ((typeof array == 'undefined') || (typeof array === 'null')) {
-    return 0;
-  }
-  else {
+
   let q = array.reduce(function(sum, cur) {
     return (sum + cur)
   })
   let w = array.length;
   let e = q / w;
-  return e;}
+  return e;
 }
